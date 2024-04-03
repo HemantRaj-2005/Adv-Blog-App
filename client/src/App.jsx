a648f739946449d95674b2ae7293a5b4f8098045
Hemant Raj <coder.hemantraj05@gmail.com>
2024-04-04 04:08:22 +0530

    complete sign in page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {Home} from './pages/Home'
-import {About} from './pages/About'
-import {SignIn} from './pages/SignIn'
-import {Dashboard} from './pages/Dashboard'
-import {Projects} from './pages/Projects'
-import {SignUp} from './pages/SignUp'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { SignIn } from './pages/SignIn'
+import { Dashboard } from './pages/Dashboard'
+import { Projects } from './pages/Projects'
+import { SignUp } from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -20,9 +21,9 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/header" element={<Header />} />
-        
       </Routes>
-        
+      <Footer />
+
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,82 @@
+
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+export default function FooterCom() {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+            <Link
+              to='/'
+              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+            >
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                404 Brain Not Found's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://lighthearted-cactus-bca341.netlify.app/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Mini project
+                </Footer.Link>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  HemOjas's Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://www.github.com/HemantRaj-2005'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='https://www.instagram.com/hemantraj_2005'>Instagram</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="HemOjas's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='https://www.facebook.com/profile.php?id=100078707586854&mibextid=ZbWKwL' icon={BsFacebook}/>
+            <Footer.Icon href='https://www.instagram.com/hemantraj_2005' icon={BsInstagram}/>
+            <Footer.Icon href='https://twitter.com/HEMANTR62340858' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com/HemantRaj-2005' icon={BsGithub}/>
+            <Footer.Icon href='#' icon={BsDribbble}/>
+
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
