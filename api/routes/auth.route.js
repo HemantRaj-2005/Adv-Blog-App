03ed4c7fc6bb20deba91396f553c94e3a524f40d
Hemant Raj <coder.hemantraj05@gmail.com>
2024-04-04 12:24:58 +0530

    google authentication working

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { signup } from '../controllers/auth.controller.js'
 import { signin } from '../controllers/auth.controller.js';
+import { google } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
