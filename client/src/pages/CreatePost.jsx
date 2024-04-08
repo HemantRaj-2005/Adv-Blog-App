b72bfccb1b3780ae5b5dfad9ffe7ada57c004553
Hemant Raj <coder.hemantraj05@gmail.com>
2024-04-08 23:33:57 +0530

    made home page and search page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -82,6 +82,7 @@ export default function CreatePost() {
             setPublishError('Something went wrong');
         }
     };
+
     return (
         <div className='p-3 max-w-3xl mx-auto min-h-screen'>
             <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
